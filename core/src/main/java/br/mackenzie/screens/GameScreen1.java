7d633cd58c758f9e077079f9af9c027ee9ab2209
JuliaDAgrella <julia.dagrela.araujo@gmail.com>
2025-11-12 08:36:17 -0300

    Foi adicionado o terceiro mapa (já está funcionando)

--- core/src/main/java/br/mackenzie/screens/GameScreen1.java
+++ core/src/main/java/br/mackenzie/screens/GameScreen1.java
@@ -8,4 +8,7 @@ public class GameScreen1 extends BaseGameScreen {
     @Override protected String getMapPath() { return "esgoto.tmx"; }
     @Override protected BaseGameScreen newInstance() { return new GameScreen1(game); }
     @Override protected BaseGameScreen getNextScreen() { return new GameScreen2(game); }
+
+    public void show() {
+        super.show();}
 }
